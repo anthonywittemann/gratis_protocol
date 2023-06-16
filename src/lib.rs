a959c3cbd668ab1f6d7714b660e579f9f20e25f8
anon <awittema@usc.edu>
2023-06-16 16:03:36 -0500

    WIP refactor

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,10 @@ use crate::external::*;
 pub mod oracle;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::borsh::maybestd::collections::{HashMap, HashSet};
+use near_sdk::borsh::maybestd::collections::{HashMap};
+use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::{
-    env, near_bindgen, AccountId, Balance, Gas, PanicOnDefault, Promise, PublicKey, StorageUsage,
+    env, near_bindgen, AccountId, Balance, Gas, PanicOnDefault, Promise, PromiseResult, PublicKey, StorageUsage,
 };
 
 use std::str::FromStr;
@@ -20,13 +21,16 @@ const LENDING_CONTRACT_ID: &str = "gratis_protocol.testnet"; // TODO: update wit
 const PRICE_ORACLE_CONTRACT_ID: &str = "price_oracle.testnet";
 const MIN_COLLATERAL_RATIO: u128 = 120;
 const LOWER_COLLATERAL_RATIO: u128 = 105;
+const DEFAULT_GAS_AMOUNT: Gas = Gas(50_000_000_000_000);
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct LendingProtocol {
-    pub loans: HashMap<AccountId, Loan>,
-    pub allowed_accounts: HashSet<AccountId>,
-    pub oracle_id: AccountId,
+pub struct LendingContract {
+    pub loans: LookupMap<AccountId, Loan>,
+    pub privaleged_accounts: UnorderedSet<AccountId>,
+    pub general_accounts: UnorderedSet<AccountId>,
+    /// The last price from the oracle
+    pub last_prices: HashMap<TokenId, Price>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -36,95 +40,90 @@ pub struct Loan {
     pub collateral_ratio: u128,
 }
 
-impl LendingProtocol {
-    pub fn new(allowed_accounts: Vec<AccountId>) -> Self {
+#[near_bindgen]
+impl LendingContract {
+    /// Initializes the contract. Should only be called once
+    #[init]
+    pub fn new(privaleged_accounts: Vec<AccountId>) -> Self {
         assert!(env::state_read::<Self>().is_none(), "Contract is already initialized");
         assert_eq!(env::predecessor_account_id(), env::current_account_id(), "Only contract owner can call this method");
     
         Self {
-            loans: HashMap::new(),
-            allowed_accounts: allowed_accounts.into_iter().collect(),
-            oracle_id: AccountId::from_str(&PRICE_ORACLE_CONTRACT_ID).unwrap(),
+            loans: LookupMap::new(),
+            privaleged_accounts: privaleged_accounts.into_iter().collect(),
+            general_accounts: UnorderedSet::new(),
+            last_prices: HashMap::new(),
         }
     }
 
-    fn get_usdt_value(&self, collateral: Balance) -> Promise {
-        // here we are assuming the collateral is in NEAR - TODO allow for other collateral types
-        // let oracle_contract_id: AccountId = AccountId::from_str(&PRICE_ORACLE_CONTRACT_ID).unwrap();
-        // let method_name: String = "get_price".to_string();
-        // let args: Vec<u8> = serde_json::to_vec("NEAR").unwrap();  // Assuming the collateral is in NEAR
-        let gas: Gas = Gas(50_000_000_000_000);
-        let deposit: Balance = 0;
-
-
-        let promise = ext_price_oracle::ext(self.oracle_id.clone())
-        .with_static_gas(gas)
-        .get_price_data(Some(vec!["NEAR".to_string()]));
-
-        return promise;
-
-        // ext_price_oracle::get_price(
-        //     "NEAR".into(),
-        //     &oracle_contract_id,
-        //     0,
-        //     gas,
-        // )
+    fn get_usdt_price(&self, asset: String) -> Promise {
+        ext_price_oracle::ext(self.oracle_id.clone())
+        .with_static_gas(DEFAULT_GAS_AMOUNT)
+        .get_price_data(Some(vec![asset]))
     }
 
-    // pub fn deposit_collateral(&mut self, mut amount: Balance) {
-    //     let fee = amount / 200; // 0.5% fee
-    //     amount -= fee;
+    pub fn deposit_collateral(&mut self, mut amount: Balance) -> Promise {
+        let fee = amount / 200; // 0.5% fee
+        amount -= fee;
 
-    //     assert!(amount > 0, "Deposit Amount should be greater than 0");
+        assert!(amount > 0, "Deposit Amount should be greater than 0");
 
-    //     let account_id = env::signer_account_id();
-    //     let loan: &mut Loan = self.loans.entry(account_id.clone()).or_insert(Loan {
-    //         collateral: 0,
-    //         borrowed: 0,
-    //         collateral_ratio: if self.allowed_accounts.contains(&account_id) {
-    //             LOWER_COLLATERAL_RATIO
-    //         } else {
-    //             MIN_COLLATERAL_RATIO
-    //         },
-    //     });
+        let account_id = env::signer_account_id();
+        let loan: &mut Loan = self.loans.entry(account_id.clone()).or_insert(Loan {
+            collateral: 0,
+            borrowed: 0,
+            collateral_ratio: if self.privaleged_accounts.contains(&account_id) {
+                LOWER_COLLATERAL_RATIO
+            } else {
+                MIN_COLLATERAL_RATIO
+            },
+        });
 
-    //     loan.collateral += amount;
-    //     Promise::new(account_id).transfer(amount);
-    // }
+        loan.collateral += amount;
+        Promise::new(account_id).transfer(amount)
+    }
 
-    // pub fn borrow(&mut self, usdt_amount: Balance) {
+    #[private]
+    pub fn borrow_callback(&mut self, 
+        #[callback_result] price_promise_result: PricePromiseResult, 
+        usdt_amount: Balance, 
+        current_account_id: AccountId, 
+        loan: &mut Loan
+    ) -> Promise {
+        let usdt_collateral_value: u128 = (price * loan.collateral) / 100;
+        let min_usdt_value: u128 = (usdt_amount * loan.collateral_ratio) / 100;
+
+        assert!(usdt_collateral_value >= min_usdt_value, "Insufficient collateral");
+
+        loan.borrowed += usdt_amount;
+        Promise::new(current_account_id).function_call(
+            "ft_transfer".to_string(),
+            format!(
+                r#"{{"receiver_id": "{}", "amount": "{}", "memo": "Borrowed USDT"}}"#,
+                current_account_id, usdt_amount
+            ).into_bytes(),
+            0,
+            DEFAULT_GAS_AMOUNT,
+        )
+    }
+
+    // pub fn borrow(&mut self, usdt_amount: Balance) -> Promise {
     //     assert!(usdt_amount > 0, "Borrow Amount should be greater than 0");
     
     //     let signer_account_id: AccountId = env::signer_account_id();
     //     let loan: &mut Loan = self.loans.get_mut(&signer_account_id).expect("No collateral deposited");
-    
-    //     // This Promise will eventually call `check_borrow` when the price is ready.
-    //     self.get_usdt_value(loan.collateral)
-    //         .then(ext_self::check_borrow(
+    //     let asset: String = "NEAR".to_string();  // TODO rm hardcoded NEAR asset and allow for others
+    //     self.get_usdt_price(asset).then(
+    //         Self::ext(env::current_account_id())
+    //         .with_static_gas(DEFAULT_GAS_AMOUNT)
+    //         .borrow_callback()
+    //         .input((
     //             usdt_amount,
-    //             loan.collateral_ratio,
-    //             &env::current_account_id(),
+    //             env::current_account_id(),
     //             loan,
-    //         ));
-        
-    // }
-
-    // fn check_borrow(&mut self, usdt_amount: Balance, price: Balance, collateral_ratio: u128, current_account_id: AccountId, loan: &mut Loan) {
-    //     let usdt_value: u128 = price * loan.collateral_ratio * loan.collateral / 100;
-    //     let min_usdt_value: u128 = (usdt_amount * loan.collateral_ratio) / 100;
-
-    //     assert!(usdt_value >= min_usdt_value, "Insufficient collateral");
-
-    //     loan.borrowed += usdt_amount;
-    //     Promise::new(current_account_id).function_call(
-    //         "ft_transfer".to_string(),
-    //         format!(
-    //             r#"{{"receiver_id": "{}", "amount": "{}", "memo": "Borrowed USDT"}}"#,
-    //             current_account_id, usdt_amount
-    //         ).into_bytes(),
-    //         0,
-    //         Gas(50_000_000_000_000),
-    //     );
+    //         ))
+    //         .gas(DEFAULT_GAS_AMOUNT)
+    //     )
     // }
 
     // // The "repay" method calculates the actual repayment amount and the refund amount based on the outstanding loan. If there's an overpayment, it will refund the excess amount to the user.
